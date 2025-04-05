3f0e29be4b136d92ba32bf4dcd098f8cd2527724
AayushKotwani3 <aayushkotwani9@gmail.com>
2025-04-06 04:33:19 +0530

    Created database entered admin create logon and register endpoints and also worked on models and templating

--- Application/controllers.py
+++ Application/controllers.py
@@ -1,2 +1,38 @@
 from flask import Flask,render_template,redirect,request
 from flask import current_app as app
+from .models import * #inheriting models module to make indirect connection with app.py
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method=='POST':
+        username=request.form.get('username')
+        password=request.form.get('password')
+        this_user=User.query.filter_by(username=username).first() #Lhs is attribite from db table and rhs is form submission value
+        if this_user:
+            if this_user.password==password: #lhs is from db table and rhs is form submission value
+                 if this_user.type=='admin':
+                    return 'Admindashboard'
+                 else:
+                    return render_template('user_dashboard.html',username=username)
+            else:
+                return 'Incorrect Password'
+        else:
+             return 'User doesnt exist'
+    return render_template('login.html')
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+    if request.method=='POST':
+        username=request.form.get('username')
+        email=request.form.get('email')
+        password=request.form.get('password')
+        this_user=User.query.filter_by(username=username).first() #Lhs is attribite from db table and rhs is form submission value
+        if this_user:
+            return 'User already registered'
+        else:
+            new_user=User(username=username,email=email,password=password)
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect('/login')
+    return render_template('Register.html')
+
--- app.py
+++ app.py
@@ -3,15 +3,21 @@ from Application.database import db
 app=None
 
 def create_app():
+    # global app
     app=Flask(__name__)
     app.debug=True
     app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///ecard.sqlite" #step 3 Database
     db.init_app(app) #step 3 Database
-    app.app_context.push()
+    with app.app_context():
+        import Application.controllers
+    # with app.app_context():
+    #     pass
     return app
 
 app=create_app()
-from Application.controllers import * #step2 controllers
+# with app.app_context():
+    #from Application.controllers import * #step2 controllers
+# from Application.models import * # making connection with models module using controllers so no need for this
 
 if __name__ == "__main__":
-    app.run
+    app.run()
